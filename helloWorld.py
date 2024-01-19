2d9de183a344be5dfd217b2336880b3aa469db96
GusWin <134251049+GusWin@users.noreply.github.com>
2024-01-19 13:16:11 +1100

    OOP Week 1

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,25 @@
 print("Welcome to the OOP Week 1")
+
+if 5 > 2:
+    print("Five is greater than two")
+
+# This is Comment
+
+# Varia ble
+name = "Jhon"
+age = 20
+
+print(name)
+print(20)
+
+# Multiple Variable
+x, y, z = "Apple","Banana","Cherry"
+print(x)
+print(y)
+print(z)
+
+# Merging Words
+word1 = "Python"
+word2 = "Is"
+word3 = "Cool"
+print(word1,word2,word3)
